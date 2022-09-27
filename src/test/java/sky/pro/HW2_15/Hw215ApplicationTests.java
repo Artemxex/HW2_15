ddac71022f34747213f0ac5b77b4ace99c110e65
Artemxex <Artem.363636@gmail.com>
2022-09-28 01:23:23 +0300

    Initial commit

---
+++ src/test/java/sky/pro/HW2_15/Hw215ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.HW2_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw215ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
